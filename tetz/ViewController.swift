b41ff4c1d52d8406fc8f713b4a82fb34dd2adf50
Dionisio <alto_lino@yahoo.com>
2018-10-25 12:49:43 -0500

    initial commit

---
+++ tetz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tetz
+//
+//  Created by desarrollo on 10/25/18.
+//  Copyright © 2018 Dionisio Del Cid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
